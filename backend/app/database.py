30f9b0187d2d019fb79b5b73f1f37109d88d7640
Francisco <frank.romo321@gmail.com>
2026-06-25 13:34:37 -0600

    Add: DB lifespan connection.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,16 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlit:///./test.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 class Base(DeclarativeBase):
     pass
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 from routers import UserRoutes, PortfolioRoutes
 
+# Async-related imports
+from contextlib import asynccontextmanager
+from fastapi.exception_handlers import http_exception_handler, websocket_request_validation_exception_handler
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+
+# Create tables in a lifespan function
+
+
 app = FastAPI()
 
 
