a9a548c3f494a7480739ad69f28ce49f2c224ebd
Francisco <frank.romo321@gmail.com>
2026-06-29 09:34:48 -0600

    add feature: delete user

--- backend/app/routers/UserRoutes.py
+++ backend/app/routers/UserRoutes.py
@@ -132,6 +132,22 @@ async def update_user(
 
 
 # ======== DELETE USER ========
-@router.delete()
-async def delete_user():
-    pass
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(
+    user_id: int,
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(
+        select(models.User).where(models.User.id == user_id)
+    )
+    
+    user = result.scalars().first()
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+        )
+    
+    await db.delete(user)
+    await db.commit()
