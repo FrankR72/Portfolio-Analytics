741afbb60fa696d8587325c30a6ae5b556611a88
Francisco <frank.romo321@gmail.com>
2026-07-10 18:55:49 -0600

    add: transactions models and schemas.

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,7 +3,7 @@ Define models for Database using SQLAlchemy for the FastAPI application.
 """
 from __future__ import annotations
 
-from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy import ForeignKey, Integer, String, Float, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from datetime import datetime
@@ -29,7 +29,7 @@ class Portfolio(Base):
     __tablename__ = "portfolios"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
     date_created: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     
     user_id: Mapped[int] = mapped_column(
@@ -37,6 +37,28 @@ class Portfolio(Base):
         nullable=False,
         index=True
     )
-    
     author: Mapped[User] = relationship(back_populates="portfolios")
-   
+    
+    transactions: Mapped[list[Transaction]] = relationship(
+        back_populates="portfolio",
+        cascade="all, delete-orphan"
+    )
+    
+    
+class Transaction(Base):
+    
+    __tablename__ = "transactions"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    symbol: Mapped[str] = mapped_column(String, nullable=False)
+    transaction_type: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    quntity_actions: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    transaction_date: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    
+    portfolio_id: Mapped[int] = mapped_column(
+        ForeignKey("portfolios.id"),
+        nullable=False,
+        index=True
+    )
+    portfolio: Mapped[Portfolio] = relationship(back_populates="transactions")
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -3,6 +3,7 @@ Define Pydantic models for request and response validation in the FastAPI applic
 """
 from pydantic import BaseModel, Field, ConfigDict, EmailStr
 
+from datetime import datetime
 
 
 # Pydantic models for User
@@ -26,7 +27,7 @@ class UserUpdate(UserBase):
 
 # Pydantic models for Portfolio
 class PortfolioBase(BaseModel):
-    title: str = Field(max_length=100, description="The name of the portfolio")
+    title: str = Field(max_length=100, description="The title of the portfolio")
     
 class PortfolioCreate(PortfolioBase):
     user_id: int #TEMPORARY
@@ -42,3 +43,28 @@ class PortfolioResponse(PortfolioBase):
 class PortfolioUpdate(PortfolioBase):
     title: str | None = Field(default=None, max_length=100)
     
+
+
+# Pydantic models for Transactions
+class TransactionBase(BaseModel):
+    symbol: str = Field(min_length=1, max_length=6, description="The symbol of the stock.")
+    transaction_type: bool
+    quantity_actions: int = Field(gt=0)
+    price: float = Field(gt=0)
+    
+class TransactionCreate(TransactionBase):
+    portfolio_id : int #TEMPORARY
+
+class TransactionResponse(TransactionBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    portfolio_id: int
+    transaction_date: datetime
+    portfolio: PortfolioResponse
+
+class TransactionUpdate(TransactionBase):
+    symbol: str | None = Field(default=None, min_length=1, max_length=6)
+    transaction_type: bool | None = Field(default=None)
+    quantity_actions: int | None = Field(default=None, gt=0)
+    price: float | None = Field(default=None, gt=0)
